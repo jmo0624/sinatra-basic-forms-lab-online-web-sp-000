5241227fbc7446f79c5d4125e4b52dc563ff8f2b
Josh Morris <jmo0624@gmail.com>
2020-03-03 00:18:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,9 +11,9 @@ class App < Sinatra::Base
   end
 
   post '/new' do
-    
-    @new_pooch = Puppy.new(params[:name], params[:breed], params[:age])
-
+    @name = params[:name]
+    @breed = params[:breed]
+    @age = params[:age]
     erb :display_puppy
   end
 end
